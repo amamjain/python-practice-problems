7d4be09d2610db284e4ff6dbc713ca0af5a13595
amamjain <114607890+amamjain@users.noreply.github.com>
2023-02-05 00:43:40 -0600

    answered

--- problems/trees/exptree.py
+++ problems/trees/exptree.py
@@ -29,13 +29,11 @@ class Plus:
     def eval(self):
         op1_value = self.op1.eval()
         op2_value = self.op2.eval()
-
         return op1_value + op2_value
 
     def __str__(self):
         op1_str = str(self.op1)
         op2_str = str(self.op2)
-
         return f"({op1_str} + {op2_str})"
 
 
@@ -53,15 +51,115 @@ class Times:
     def eval(self):
         op1_value = self.op1.eval()
         op2_value = self.op2.eval()
-
         return op1_value * op2_value
 
     def __str__(self):
         op1_str = str(self.op1)
         op2_str = str(self.op2)
-
         return f"({op1_str} * {op2_str})"
+    
+    
+class Minus:
+    def __init__(self, op1, op2):
+        self.op1 = op1
+        self.op2 = op2
 
+    def is_const(self):
+        return False
+    
+    def num_nodes(self):
+        return 1 + self.op1.num_nodes() + self.op2.num_nodes()
+    
+    def eval(self):
+        op1_value = self.op1.eval()
+        op2_value = self.op2.eval()
+        return op1_value - op2_value
+    
+    def __str__(self):
+        op1_str = str(self.op1)
+        op2_str = str(self.op2)
+        return f"({op1_str} - {op2_str})"
+    
+    
+ class FloorDivide:
+    def __init__(self, op1, op2):
+        self.op1 = op1
+        self.op2 = op2
+
+    def is_const(self):
+        return False
+    
+    def num_nodes(self):
+        return 1 + self.op1.num_nodes() + self.op2.num_nodes()
+    
+    def eval(self):
+        op1_value = self.op1.eval()
+        op2_value = self.op2.eval()
+        return op1_value // op2_value
+    
+    def __str__(self):
+        op1_str = str(self.op1)
+        op2_str = str(self.op2)
+        return f"({op1_str} // {op2_str})"
+    
+    
+class Abs:
+    def __init__(self, op1):
+        self.op1 = op1
+
+    def is_const(self):
+        return False
+    
+    def num_nodes(self):
+        return 1 + self.op1.num_nodes()
+    
+    def eval(self):
+        op1_value = self.op1.eval()
+        return abs(op2_value)
+    
+    def __str__(self):
+        op1_str = str(self.op1)
+        return f"(|{op1_str}|)"
+    
+    
+#REDESIGNED
+class BinOp:
+    def __init__(self, operator, op1, op2):
+        self.op1 = op1
+        self.op2 = op2
+        self.operator = operator
+        assert operator == "plus" or "times" or "minus" or "floor divide" 
+
+    def is_const(self):
+        return False
+    
+    def num_nodes(self):
+        return 1 + self.op1.num_nodes() + self.op2.num_nodes()
+    
+    def eval(self):
+        op1_value = self.op1.eval()
+        op2_value = self.op2.eval()
+        if self.operator == "plus":
+            return op1_value + op2_value
+        if self.operator == "minus":
+            return op1_value - op2_value
+        if self.operator == "times":
+            return op1_value * op2_value
+        else:
+            return op1_value // op2_value
+    
+    def __str__(self):
+        op1_value = self.op1.eval()
+        op2_value = self.op2.eval()
+        if self.operator == "plus":
+            return f"({op1_str} + {op2_str})"
+        if self.operator == "minus":
+            return f"({op1_str} - {op2_str})"
+        if self.operator == "times":
+            return f"({op1_str} * {op2_str})"
+        else:
+            return f"({op1_str} // {op2_str})"
+    
 
 if __name__ == "__main__":
 
